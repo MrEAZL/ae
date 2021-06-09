a6b65a66a28f99434bd604df4cee125afaae2316
MrEAZL <51537507+MrEAZL@users.noreply.github.com>
2021-06-09 22:52:09 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,22 @@
+--[[
+	@Author: Anna W. Anna W. <https://devforum.roblox.com/u/ImActuallyAnna> Skylar L. <https://devforum.roblox.com/u/ScobayDu>
+	@Description: RBXM File Reader
+	@Date of Creation: 09. 05. 2020
+	Copyright (C) 2020 Kat Digital Limited.
+	 
+	This program is free software: you can redistribute it and/or modify  
+	it under the terms of the GNU General Public License as published by  
+	the Free Software Foundation, version 3.
+	
+	This program is distributed in the hope that it will be useful, but 
+	WITHOUT ANY WARRANTY; without even the implied warranty of 
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+	General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License 
+	along with this program. If not, see <http://www.gnu.org/licenses/>
+--]]
+
 --Module to handle binary encoding and decoding
 local Binary = {}
 
